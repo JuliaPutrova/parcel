32d8bd217fdf596f5bdfbc8337354defd74c403e
JuliaPutrova <Julia.putrova@gmail.com>
2021-07-15 22:28:13 +0300

    added gallery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,179 @@
 import './sass/main.scss';
+
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+  modal: document.querySelector(".js-lightbox"),
+  modalImg: document.querySelector(".lightbox__image"),
+};
+
+let activeIndex = null;
+
+// ======================== Step 1 - converting incoming data into Markup ==============================
+
+// //получаю доступ к родителю галереи:
+
+// const gallery = document.querySelector(".js-gallery");
+
+// переменная для функции создающей разметку:
+
+const imgCardsMarkup = createImgGalleryMarkup(galleryItems);
+
+refs.gallery.insertAdjacentHTML("beforeend", imgCardsMarkup.join(""));
+
+// функция, которая преображает входящий массив объектов в разметку:
+
+function createImgGalleryMarkup(galleryItems) {
+  return galleryItems.map((galleryItem) => {
+    return `<li class="gallery__item">
+                <a
+                    class="gallery__link"
+                    href="${galleryItem.original}"
+                >
+                <img
+                    class="gallery__image"
+                    src="${galleryItem.preview}"
+                    data-source="${galleryItem.original}"
+                    alt="${galleryItem.description}"
+                    />
+                </a>
+            </li>`;
+  });
+}
+
+// ================================ Step 2 - open && close modal =======================================================
+
+refs.gallery.addEventListener("click", onGalleryClick);
+
+// proverka
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  imgCardsMarkup.forEach((element, index) => {
+    if (element.includes(event.target.src)) {
+      activeIndex = index;
+    }
+  });
+  console.log(activeIndex);
+  window.addEventListener("keydown", keyboardManipulation);
+  refs.modal.classList.add("is-open");
+  refs.modalImg.src = event.target.dataset.source;
+}
+
+// function closeModal(event) {
+//   if (event.target.nodeName === "IMG") {
+//     return;
+//   }
+//   refs.modal.classList.remove("is-open");
+//   refs.modalImg.src = "#";
+// }
+function closeModal() {
+  refs.modal.classList.remove("is-open");
+  refs.modalImg.src = "";
+  refs.modalImg.alt = "";
+  window.removeEventListener("keyup", keyboardManipulation);
+}
+
+refs.modal.addEventListener("click", closeModal);
+// window.addEventListener("keydown", (event) => {
+//   if (event.key === "Escape") {
+//     closeModal(event);
+//   }
+//   return;
+// });
+
+// ================================ Step 3 - switch with arrows =======================================================//
+
+function keyboardManipulation(e) {
+  switch (e.key) {
+    case activeIndex < galleryItems.length - 1 && "ArrowRight":
+      activeIndex += 1;
+      refs.modalImg.src = galleryItems[activeIndex].original;
+      break;
+    case activeIndex > 0 && "ArrowLeft":
+      activeIndex -= 1;
+      refs.modalImg.src = galleryItems[activeIndex].original;
+      break;
+    case activeIndex === 0 && "ArrowLeft":
+      activeIndex = galleryItems.length - 1;
+      refs.modalImg.src = galleryItems[activeIndex].original;
+      break;
+    case activeIndex === galleryItems.length - 1 && "ArrowRight":
+      activeIndex = 0;
+      refs.modalImg.src = galleryItems[activeIndex].original;
+      break;
+    case "Escape":
+      closeModal();
+      break;
+    default:
+      alert("что-то пошло не так");
+  }
+}
